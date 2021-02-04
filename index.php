343e26fdc0f28f67921ea2cb3bfd4b7a02ab41a7
sampurna-d <sampurnadhungana@gmail.com>
2021-02-04 15:27:07 -0500

    lab 2

---
+++ index.php
@@ -0,0 +1,53 @@
+<main>
+<body>
+    <link rel = "stylesheet" href= "css/about.css">
+
+    <div class = "about">
+        <h1> About Us</h1>
+        <p> Our Team</p>
+    </div>
+    <div class = "row">
+        <div class="column">
+            <div class="card">
+                <img src="images/ceo.jpg">
+                <div class = "container">
+                    <h2>CEO</h2>
+                    <p>Nolan</p>
+                    <p>Email: cnolan@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/leo.jpeg">
+                <div class = "container">
+                    <h2>Manger</h2>
+                    <p>Leo Carp</p>
+                    <p>Email: leo@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/critic.jpg">
+                <div class = "container">
+                    <h2>Critic</h2>
+                    <p>Rajesh Hal</p>
+                    <p>Email: rajesh@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/emma.jpg">
+                <div class = "container">
+                    <h2>Writer</h2>
+                    <p>Emily Tert</p>
+                    <p>Email: emm@gmail.com</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </body>
+</main>
