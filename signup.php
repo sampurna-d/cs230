84aced7b3b17af34acd0919fbde258a4930eac9e
sampurna-d <sampurnadhungana@gmail.com>
2021-02-19 19:08:23 -0500

    Lab4

--- signup.php
+++ signup.php
@@ -7,42 +7,43 @@ require "includes/header.php"
     <div class="bg-cover">
         <div class="h-100 container center-me">
             <div class="my-auto">
-                <div class = signup-form>
-                    <form action="include/signup-helper.php" methods="post">
+                <div class="signup-form">
 
-    
-                        <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
-                        <p class="hint-text">
-                        Create your account!
-                        </p>
+                    <form action="includes/signup-helper.php" method="post">
+
+
+                        <h1 class="h3 mb-3 fw-normal">Please Sign Up</h1>
+                        <p class="hint-text">Create your account!</p>
                         <div class="form-group">
                             <div class="row">
-                                <div class ="col">
-                                    <input type="text" class="form-control" name="fname"placeholder="First Name" required autofocus>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="fname" placeholder="First Name" required autofocus>
                                 </div>
-                                <div class ="col">
-                                    <input type="text" class="form-control" name="lname"placeholder="Last Name" required autofocus>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="lname" placeholder="Last Name" required autofocus>
                                 </div>
                             </div>
                         </div>
-                        <input type="text" class="form-control" name="uname" placeholder="User Name" required autofocus>
+
+                        <input type="text" class="form-control" name="uname" placeholder="Username" required autofocus>
 
                         <label for="inputEmail" class="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" class="form-control"name="email" placeholder="Email address" required autofocus>
-                        
-                        <label for="inputPassword" class="sr-only">Password</label>
-                        <input type="password" id="inputPassword" class="form-control"name="pwd" placeholder="Password" required>
-                        
-                        <label for="inputPassword" class="sr-only">Confirm Password</label>
-                        <input type="password" id="inputPassword" class="form-control"name="con-pwd" placeholder="Confirm Password" required>
-      
-                        <button class="btn btn-lg btn-outline-danger btn-block"name="signup-submit" type="submit">Sign in</button>
-                        <p class="mt-5 mb-3 text-muted">&copy; 2020–9999</p>
-                    </form>
-                </div>
+                        <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required autofocus>
 
+                        <label for="inputPassword" class="sr-only">Password</label>
+                        <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required>
+
+                        <label for="inputPassword" class="sr-only">Confirm Password</label>
+                        <input type="password" id="inputPassword" class="form-control" name="con-pwd" placeholder="Confirm Password" required>
+
+                        <button class="btn btn-lg btn-outline-danger btn-block" name="signup-submit" type="submit">Sign in</button>
+                        <p class="mt-5 mb-3 text-muted">&copy; 2020–9999</p>
+
+                    </form>
+
+                </div>
             </div>
         </div>
     </div>
-    
+
 </main>
